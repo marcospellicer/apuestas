39a764175cafdeba3dc6a1a8e35dac70c782f492
Marcos <Marcos@MARCOS-PC>
2018-03-14 02:28:50 +0100

    terminado

--- Apuestas/src/apuestas/Persona.java
+++ Apuestas/src/apuestas/Persona.java
@@ -10,5 +10,78 @@ package apuestas;
  * @author alumno
  */
 public class Persona {
+    private String nombre;
+    private int golL;
+    private int golV;
+    private boolean pagado;
+
+    public Persona(String nombre, int golL, int golV, boolean pagado) {
+        this.nombre = nombre;
+        this.golL = golL;
+        this.golV = golV;
+        this.pagado = pagado;
+    }
+
+    @Override
+    public String toString() {
+        return   getNombre() + " --> Local " + getGolL() + " - " + getGolV() + " Visitante " + "     Pagado : "+isPagado();
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the golL
+     */
+    public int getGolL() {
+        return golL;
+    }
+
+    /**
+     * @param golL the golL to set
+     */
+    public void setGolL(int golL) {
+        this.golL = golL;
+    }
+
+    /**
+     * @return the golV
+     */
+    public int getGolV() {
+        return golV;
+    }
+
+    /**
+     * @param golV the golV to set
+     */
+    public void setGolV(int golV) {
+        this.golV = golV;
+    }
+
+    /**
+     * @return the pagado
+     */
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    /**
+     * @param pagado the pagado to set
+     */
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+    
     
 }
